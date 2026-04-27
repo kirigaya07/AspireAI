6e3d94dfad8ef6f24772c13f614effc005a09685
kirigaya07 <crimsomemoon7@gmail.com>
2026-04-28 01:08:15 +0530

    Add Inngest payment reconciliation job for stuck pending payments

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,14 +1,10 @@
 export const dynamic = "force-dynamic";
 
 import { inngest } from "@/lib/inngest/client";
-import { generateIndustryInsights } from "@/lib/inngest/functions";
+import { generateIndustryInsights, reconcilePendingPayments } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
 
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    /* your functions will be passed here later! */
-    generateIndustryInsights,
-  ],
+  functions: [generateIndustryInsights, reconcilePendingPayments],
 });
